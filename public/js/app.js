38deab044df50085c5de78cc99091261560edb50
Wainii Dev <wainii1998@gmail.com>
2024-04-05 22:09:44 +0700

    Update style

--- public/js/app.js
+++ public/js/app.js
@@ -26,39 +26,39 @@ function LoadImage(elm) {
 }
 
 // Animation Logo
-let heading = document.querySelector("#wainiidev-logo a");
-let count = 0;
-let countTwo = heading.dataset.title.length;
+// let heading = document.querySelector("#wainiidev-logo a");
+// let count = 0;
+// let countTwo = heading.dataset.title.length;
 
-setTimeout(myFunc, 1000);
+// setTimeout(myFunc, 1000);
 
-function myFunc() {
-    let name = heading.dataset.title;
-    heading.innerHTML = "";
+// function myFunc() {
+//     let name = heading.dataset.title;
+//     heading.innerHTML = "";
 
-    setInterval(() => {
-        runFunc();
-    }, 1200);
+//     setInterval(() => {
+//         runFunc();
+//     }, 1200);
 
-    function runFunc() {
-        if (count < name.length) {
-            let createSpan = document.createElement("span");
-            createSpan.setAttribute("class", " ");
-            createSpan.setAttribute("class", "animationOne");
-            createSpan.innerHTML = name.charAt(count);
-            heading.append(createSpan);
-            count++;
-        } else {
-            heading.children[countTwo - 1].className = "animationTwo";
-            countTwo--;
-            if (countTwo <= 0) {
-                heading.innerHTML = " ";
-                count = 0;
-                countTwo = heading.dataset.title.length;
-            }
-        }
-    }
-}
+//     function runFunc() {
+//         if (count < name.length) {
+//             let createSpan = document.createElement("span");
+//             createSpan.setAttribute("class", " ");
+//             createSpan.setAttribute("class", "animationOne");
+//             createSpan.innerHTML = name.charAt(count);
+//             heading.append(createSpan);
+//             count++;
+//         } else {
+//             heading.children[countTwo - 1].className = "animationTwo";
+//             countTwo--;
+//             if (countTwo <= 0) {
+//                 heading.innerHTML = " ";
+//                 count = 0;
+//                 countTwo = heading.dataset.title.length;
+//             }
+//         }
+//     }
+// }
 
 
 // Carousel topic
